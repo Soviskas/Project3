6bed751910504c3d961363597a0315b56aa36345
Soviskas <kisilovoleksandr48@gmail.com>
2024-05-12 17:00:00 +0300

    main.cpp

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -7,14 +7,14 @@ int main() {
     std::cin >> r;
 
     std::vector<std::vector<long long>> dp(r + 1, std::vector<long long>(3, 0));
-    dp[1][1] = 2; // два числа з одного розряду: 5 і 9
+    dp[1][1] = 2; 
 
     for (int i = 2; i <= r; i++) {
-        dp[i][1] = dp[i - 1][1] + dp[i - 1][2]; // числа, які закінчуються на одну однакову цифру
-        dp[i][2] = dp[i - 1][1]; // числа, які закінчуються на дві однакові цифри
+        dp[i][1] = dp[i - 1][1] + dp[i - 1][2]; 
+        dp[i][2] = dp[i - 1][1]; 
     }
 
-    long long result = dp[r][1] + dp[r][2]; // сума чисел, які закінчуються на одну або дві однакові цифри
+    long long result = dp[r][1] + dp[r][2]; 
 
     std::cout << "Quantity of numbers from " << r << " discharges: " << result << std::endl;
 
